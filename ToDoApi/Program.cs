7a787e8f1351da792e4f88618888a85c512daaa2
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-01-24 10:35:47 +0200

    Add db to project

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,8 +1,17 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseMySql("server=localhost;database=TodoItemList;user=user;password=1234", serverVersion));
+//builder.Services.AddDbContext<TodoContext>(opt =>
+//    opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
